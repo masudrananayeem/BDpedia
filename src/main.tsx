a242f796bb376d736a4ad188e2d8bcc3cd9a6f1f
masudrananayeem <sadnayeem6393@gmail.com>
2026-07-31 12:26:10 +0600

    add ThemeProvider to main.tsx for context management

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,3 @@
 import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './App.tsx'; import './index.css';
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
+import { ThemeProvider } from './context/ThemeContext.tsx';
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><ThemeProvider><App /></ThemeProvider></React.StrictMode>);
