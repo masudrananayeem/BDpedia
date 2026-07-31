829e1cc8f193831878cac24dbc9acdef93d464b6
masudrananayeem <sadnayeem6393@gmail.com>
2026-07-31 12:25:09 +0600

    updating feature and fix bugs

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import useHashScroll from '../hooks/useHashScroll';
 
 export default function RootLayout() {
   const location = useLocation();
   const isHome = location.pathname === '/';
+  useHashScroll();
   return (
-    <div className="flex flex-col min-h-screen bg-[#0a0a0a]">
+    <div className="flex flex-col min-h-screen bg-base">
       <Navbar isHome={isHome} />
       <main className={`flex-grow ${!isHome ? 'pt-24' : ''}`}>
         <Outlet />
