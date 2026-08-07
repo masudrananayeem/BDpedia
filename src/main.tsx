4f4668b7705cede246061f434f13604e17c45c15
masudrananayeem <sadnayeem6393@gmail.com>
2026-08-07 23:47:48 +0600

    updating the features

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
 import React from 'react'; import ReactDOM from 'react-dom/client'; import App from './App.tsx'; import './index.css';
 import { ThemeProvider } from './context/ThemeContext.tsx';
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><ThemeProvider><App /></ThemeProvider></React.StrictMode>);
+import { ItineraryProvider } from './context/ItineraryContext.tsx';
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><ThemeProvider><ItineraryProvider><App /></ItineraryProvider></ThemeProvider></React.StrictMode>);
