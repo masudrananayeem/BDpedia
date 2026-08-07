d2f824ea727428e9970503a5cc0ff382117e033a
masudrananayeem <sadnayeem6393@gmail.com>
2026-08-08 02:28:45 +0600

    Fix chunk size warning, split vendor chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins: [react()] });
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'firebase-vendor': ['firebase/app', 'firebase/auth'],
+        },
+      },
+    },
+  },
+});
